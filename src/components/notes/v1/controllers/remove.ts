8f5479570f5f922e0a54332805258c96fc92432d
Kelvym Miranda <kelvymm@gmail.com>
2024-03-05 22:47:40 -0300

    feat: Change remove note route to use mongodb

--- src/components/notes/v1/controllers/remove.ts
+++ src/components/notes/v1/controllers/remove.ts
@@ -1,7 +1,31 @@
+import { client } from '@/connection'
 import { Request, Response } from 'express'
+import { ObjectId } from 'mongodb'
+import { z } from 'zod'
 
-export const remove = (req: Request, res: Response) => {
-  const id = req.params.id
+const paramsSchema = z.object({
+  id: z.string(),
+})
 
-  res.status(200).send('Note removed: ' + id)
+export const remove = async (req: Request, res: Response) => {
+  const validationParams = paramsSchema.safeParse({
+    id: req.params.id,
+  })
+
+  if (!validationParams.success) {
+    res.status(400).send(validationParams.error)
+
+    return
+  }
+
+  const { id } = validationParams.data
+
+  await client
+    .db('k-tasks')
+    .collection('notes')
+    .deleteOne({
+      _id: new ObjectId(id),
+    })
+
+  res.status(200).send({})
 }
